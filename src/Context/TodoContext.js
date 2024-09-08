e0cf5694aaaeda84f9f487ad760cf29847261bc1
bipan101 <67128157+Bipan101@users.noreply.github.com>
2024-09-08 20:57:49 +0545

    On production phase

---
+++ src/Context/TodoContext.js
@@ -0,0 +1,11 @@
+import {createContext, useContext} from 'react' 
+
+export const TodoContext = createContext()
+
+export const useTodo =()=>{
+    return useContext(TodoContext)
+}
+
+
+
+export const Todoprovider = TodoContext.Provider
